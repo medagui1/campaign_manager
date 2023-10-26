9b2fb0c690fda7075d8e6952e4d79e974d7191b5
Med Agui <simohamedregragui2@gmail.com>
2023-10-26 10:34:00 +0100

    setting the frontend directory

--- backend/campaigns/admin.py
+++ backend/campaigns/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Campaign, Subscriber
 
+class CampaignModelAdmin(admin.ModelAdmin) :
+    list_display = ("title", "created_at", "updated_at")
+    search_fields = ("title","description")
+
 # Register your models here.
-admin.site.register(Campaign)
+admin.site.register(Campaign, CampaignModelAdmin)
 admin.site.register(Subscriber)
